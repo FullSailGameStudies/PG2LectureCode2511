d3ece9e2716148e30c4f732c99141e04297e413b
Garrett Girod <girodg@hotmail.com>
2025-10-27 10:11:16 -0400

    hello

--- Week 1/Day01/Day01/Day01.cpp
+++ Week 1/Day01/Day01/Day01.cpp
@@ -21,6 +21,8 @@ void print(const std::vector<std::string>& names)
 
 int main()
 {
+    std::cout << "Hello PG2 2511\n";
+
     //Input and Console examples   
     // 
     //std::string myName = Input::GetString("What is your name?");
